6cdfcb5fb0a36d4b3ee89f5f31119aaaca827a08
Kir4Kun <rkbl4ze@gmail.com>
2022-09-21 13:49:26 +0530

    Adds modal

---
+++ components/AddEntryModal.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Dialog } from '@headlessui/vue'
+
+defineProps<{
+  show: boolean
+  onClose: () => void
+}>()
+</script>
+
+<template>
+  <Dialog :open="show" class="relative z-50" @close="onClose">
+    <!-- The backdrop, rendered as a fixed sibling to the panel container -->
+    <div class="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+    <!-- Full-screen container to center the panel -->
+    <div class="fixed inset-0 flex items-center justify-center">
+      <!-- The actual dialog panel -->
+      <DialogPanel class="modal-box w-full bg-white">
+        <h3 class="font-bold text-lg">
+          Congratulations random Internet user!
+        </h3>
+        <p class="py-4">
+          You've been selected for a chance to get one year of subscription to use Wikipedia for free!
+        </p>
+        <div class="modal-action">
+          <button class="btn btn-ghost" @click="onClose">
+            Close
+          </button>
+        </div>
+
+        <!-- ... -->
+      </DialogPanel>
+    </div>
+  </Dialog>
+</template>
--- pages/categories.vue
+++ pages/categories.vue
@@ -2,6 +2,7 @@
 import { storeToRefs } from 'pinia'
 
 import IcOutlineRemoveRedEye from '~icons/ic/outline-remove-red-eye'
+import AddEntryModal1 from '~~/components/AddEntryModal.vue'
 
 definePageMeta({
   middleware: 'auth',
@@ -10,6 +11,12 @@ definePageMeta({
 const entriesStore = useEntriesStore()
 const { entries, loading } = storeToRefs(entriesStore)
 await entriesStore.initEntries()
+
+const isOpen = ref(false)
+
+function setIsOpen(value: boolean) {
+  isOpen.value = value
+}
 </script>
 
 <template>
@@ -17,61 +24,71 @@ await entriesStore.initEntries()
     <p v-if="loading">
       Loading....
     </p>
-    <Table v-for="entry in entries" :key="entry.id">
-      <template #header>
-        <TableHeadItem>
-          #
-        </TableHeadItem>
-        <TableHeadItem>
-          Name
-        </TableHeadItem>
-        <TableHeadItem>
-          Template
-        </TableHeadItem>
-        <TableHeadItem>
-          Syllabus
-        </TableHeadItem>
-        <TableHeadItem>
-          Activated
-        </TableHeadItem>
-        <TableHeadItem>
-          Actions
-        </TableHeadItem>
-      </template>
-      <template #body>
-        <TableRow v-for="(category, i) in entry.categories" :key="category.id">
-          <TableItem>
-            {{ i + 1 }}.
-          </TableItem>
-          <TableItem>
-            <div class="flex items-center space-x-3">
-              <div class="avatar">
-                <div class="mask mask-squircle w-12 h-12">
-                  <img :src="entry.iconUrl">
+    <div v-else class="w-full">
+      <div class="flex items-center justify-between m-2 my-4">
+        <p class="text-3xl font-bold">
+          Categories ({{ entries.reduce((acc, entry) => acc + entry.categories.length, 0) }})
+        </p>
+        <button class="btn btn-primary" @click="() => setIsOpen(true)">
+          New Entry / Category
+        </button>
+      </div>
+      <AddEntryModal :show="isOpen" :on-close="() => setIsOpen(false)" />
+      <Table v-for="entry in entries" :key="entry.id">
+        <template #header>
+          <TableHeadItem>
+            #
+          </TableHeadItem>
+          <TableHeadItem>
+            Name
+          </TableHeadItem>
+          <TableHeadItem>
+            Template
+          </TableHeadItem>
+          <TableHeadItem>
+            Syllabus
+          </TableHeadItem>
+          <TableHeadItem>
+            Activated
+          </TableHeadItem>
+          <TableHeadItem>
+            Actions
+          </TableHeadItem>
+        </template>
+        <template #body>
+          <TableRow v-for="(category, i) in entry.categories" :key="category.id">
+            <TableItem>
+              {{ i + 1 }}.
+            </TableItem>
+            <TableItem>
+              <div class="flex items-center space-x-3">
+                <div class="avatar">
+                  <div class="mask mask-squircle w-12 h-12">
+                    <img :src="entry.iconUrl">
+                  </div>
+                </div>
+                <div class="font-bold">
+                  {{ category.name }}
                 </div>
               </div>
-              <div class="font-bold">
-                {{ category.name }}
-              </div>
-            </div>
-          </TableItem>
-          <TableItem>
-            <button class="btn gap-1 btn-ghost btn-sm">
-              <IcOutlineRemoveRedEye />
-              View
-            </button>
-          </TableItem>
-          <TableItem>
-            <button class="btn gap-1 btn-ghost btn-sm">
-              <IcOutlineRemoveRedEye />
-              View
-            </button>
-          </TableItem>
-          <TableItem>
-            <input type="checkbox" class="toggle toggle-sm toggle-primary" checked>
-          </TableItem>
-          <TableItem>
-            <ActionButton />
+            </TableItem>
+            <TableItem>
+              <button class="btn gap-1 btn-ghost btn-sm">
+                <IcOutlineRemoveRedEye />
+                View
+              </button>
+            </TableItem>
+            <TableItem>
+              <button class="btn gap-1 btn-ghost btn-sm">
+                <IcOutlineRemoveRedEye />
+                View
+              </button>
+            </TableItem>
+            <TableItem>
+              <input type="checkbox" class="toggle toggle-sm toggle-primary" checked>
+            </TableItem>
+            <TableItem>
+              <ActionButton />
             <!-- <div class="flex gap-2">
               <button class="btn btn-ghost btn-circle btn-sm">
                 <MaterialSymbolsEdit />
@@ -80,9 +97,10 @@ await entriesStore.initEntries()
                 <IcRoundDelete />
               </button>
             </div> -->
-          </TableItem>
-        </TableRow>
-      </template>
-    </Table>
+            </TableItem>
+          </TableRow>
+        </template>
+      </Table>
+    </div>
   </div>
 </template>
